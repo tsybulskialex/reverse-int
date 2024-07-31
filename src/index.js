6783917427b80d27ab67dbf1e04caf6854f29202
Aleksandr <tsybulski.alex@mail.ru>
2024-07-31 09:28:23 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const integer = Math.abs(n);
+    const splitString = String(integer).split('');
+    let result = '';
+    for (let i = 0; i < splitString.length; i++) {
+        result = splitString[i] + result;
+    }
+    return +result;
 }
